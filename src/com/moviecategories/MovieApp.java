3f3e2131eeb0226fdb2a8a2e73ff834c42f0283a
Julia <dart1ju@gmail.com>
2018-07-30 09:07:50 -0400

    used MovieIO class and added comments

--- src/com/moviecategories/MovieApp.java
+++ src/com/moviecategories/MovieApp.java
@@ -8,6 +8,7 @@ public class MovieApp {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		// Creating ArrayList to store first set of movies
 		ArrayList<Movie> movieList = new ArrayList<Movie>();
 		movieList.add(new Movie("X-Men", "action"));
 		movieList.add(new Movie("The Lion King", "animated"));
@@ -22,30 +23,40 @@ public class MovieApp {
 		movieList.add(new Movie("The Godfather", "drama"));
 		movieList.add(new Movie("The Shawshank Redemption", "drama"));
 
+		// adds 100 movies from MovieIO class
+		for (int i = 0; i <= 100; i++) {
+
+			movieList.add(MovieIO.getMovie(i));
+		}
+
+		// variable to continue
 		String cont = "y";
 
 		System.out.println("Welcome to the Movie Application");
 		System.out.println("There are 12 movies in this list.");
 		
+		// While loop to continue displaying the list of movies if user desires
 		while (cont.equals("y")) {
-			System.out.println("Which category would you like? (animated, action, drama, scifi)");
+			System.out.println("Which category would you like? (animated, musical, drama, scifi, horror)");
 			String userInput = scan.nextLine();
-			if (!userInput.equals("scifi") || !userInput.equals("action") || !userInput.equals("animated")
-					|| !userInput.equals("drama")) {
+			// Test to validate input
+			if (!userInput.equals("scifi") || !userInput.equals("horror") || !userInput.equals("animated")
+					|| !userInput.equals("drama") || !userInput.equals("musical")) {
 				System.out.println("Please enter a valid category");
 			}
-
+			// For loop will extract movie titles based on the category selected
+			// by user(i.e. drama or action..)
 			for(Movie m:movieList) {
 				if (m.getCategory().equalsIgnoreCase(userInput)) {
 				System.out.println(m.getTitle());
 				}
 			}
-			
+			// Prompts user to continue
 			System.out.println("Would you like to continue? (y/n)");
 			cont = scan.nextLine();
 		
 		}
-		
+		scan.close();
 	}
 
 }
